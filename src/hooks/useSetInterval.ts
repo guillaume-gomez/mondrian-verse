f08b943e140d93f0a5496123e193f150b14d88c2
Guillaume Gomez <guillaume.gomez846@gmail.com>
2022-07-01 00:34:33 +0200

    create custom hooks to deals with set interval

---
+++ src/hooks/useSetInterval.ts
@@ -0,0 +1,30 @@
+import { useState, useRef } from 'react';
+
+
+function useSetInterval() {
+  const refFunction = useRef<null|NodeJS.Timeout>(null);
+  const [isIntervalRunning, setIsIntervalRunning] = useState<boolean>(false);
+
+  function startInterval(callback: Function, timer: number) {
+    if(refFunction.current) {
+      stopInterval();
+    }
+    setIsIntervalRunning(true);
+    const ref = setInterval(() => callback(), timer);
+    refFunction.current = ref;
+  }
+
+  function stopInterval() {
+    if(!refFunction.current) {
+      return;
+    }
+    clearTimeout(refFunction.current);
+    refFunction.current = null;
+    setIsIntervalRunning(false);
+  }
+
+  return { startInterval, stopInterval, isIntervalRunning }
+
+}
+
+export default useSetInterval;
