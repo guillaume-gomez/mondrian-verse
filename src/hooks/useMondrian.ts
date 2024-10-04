420dfcb7b933ac43d2727aa7e31159dae7eb2c61
Guillaume Gomez <guillaume.gomez846@gmail.com>
2024-10-04 14:45:30 +0200

    try new algorithm based on the golden square

--- src/hooks/useMondrian.ts
+++ src/hooks/useMondrian.ts
@@ -24,6 +24,7 @@ const defaultColors = [
     ...possibleColors
 ];
 
+const Phi = 1.618003987;
 
 function useMondrian() {
   const [rects, setRects] = useState<CustomRect[]>([]);
@@ -52,6 +53,66 @@ function useMondrian() {
       accRects.push(rect);
     }
   }
+
+  function generateMondrianGoldenSquare(
+      rect: CustomRect,
+      xPad: number,
+      yPad: number,
+      accRects: CustomRect[],
+      depth: number = 0,
+      limit: number = 1
+  ) {
+      if (depth === limit) {
+        // dangerous :)
+        accRects.push(rect);
+        return;
+      }
+
+      if(limit === 1) {
+        // initial step
+        const rectsArray = splitRects(rect, xPad, yPad);
+        if(rectsArray.length === 2) {
+          generateMondrianGoldenSquare(rectsArray[0], xPad, yPad, accRects, depth + 1, limit);
+          generateMondrianGoldenSquare(rectsArray[1], xPad, yPad, accRects, depth + 1, limit);
+        } else {
+          accRects.push(rect);
+        }
+      } else {
+        const rectsArray = splitRectsGoldenSquare(rect, xPad, yPad);
+        if(rectsArray.length === 2 ) {
+          generateMondrianGoldenSquare(rectsArray[0], xPad, yPad, accRects, depth + 1, limit);
+          generateMondrianGoldenSquare(rectsArray[1], xPad, yPad, accRects, depth + 1, limit);
+        } else {
+          accRects.push(rect);
+        }
+      }
+  }
+
+    function splitRectsGoldenSquare(rect: CustomRect, xPad: number, yPad: number) : [CustomRect, CustomRect] | [] {
+     // Check the rectangle is enough large and tall
+     const width = widthRect(rect);
+     const height = heightRect(rect);
+      if (width < 2 * xPad || height < 2 * yPad) {
+          return [];
+      }
+      const { x1, x2, y1, y2 } = rect;
+
+
+      // If the rectangle is wider than it's height do a left/right split
+      if (width > height) {
+          const x = Math.ceil(width / Phi);
+          const r1 = { x1, y1, x2: cut, y2, color: randomColor() };
+          const r2 = { x1: cut, y1, x2, y2, color: randomColor() };
+          return [r1, r2];
+      // Else do a top/bottom split
+      } else {
+          const y = Math.ceil(height / Phi);
+          const r1 = { x1, y1, x2, y2: cut, color: randomColor() };
+          const r2 = { x1, y1: cut, x2, y2, color: randomColor() };
+          return [r1, r2];
+      }
+  }
+
 
   function splitRects(rect: CustomRect, xPad: number, yPad: number) : [CustomRect, CustomRect] | [] {
      // Check the rectangle is enough large and tall
